c69710b866f9fbec35faee3108bc2b60e7e26135
Stevie <steviepinero@gmail.com>
2015-10-16 02:50:26 -0400

    game math

--- _js/game.js
+++ _js/game.js
@@ -7,8 +7,13 @@ BubbleShoot.Game = (function($){
   var startGame = function(){
     $(".but_start_game").unbind("click");
     BubbleShoot.ui.hideDialog();
+    curBubble = getNextBubble();
+    $("game").bind("click",clickGameScreen);
+    };
+    // will recieve event object 'e' that gives coordinates of click
+    var clickGameScreen = function(e){
+      var angle = BubbleShoot.ui.getBubbleAngle(curBubble.getSprite(),e);
     };
   };
   return Game;
 })(jQuery);
- 
--- _js/ui.js
+++ _js/ui.js
@@ -1,10 +1,34 @@
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.ui = (function($){
   var ui = {
+    BUBBLE_DIMS : 44,
     init : function(){
     },
     hideDialog : function(){
       $(".dialog").fadeout(300);
+    }, //gets mouse coordiantes
+    getMouseCoords : function(e){
+      var coords = {x : e.pageX, y : e.pageY};
+      return coords;
+    }, //gets bubble coordinates
+    getBubbleCoords : function(bubble){
+      var bubbleCoords = bubble.position();
+      bubbleCoords.left += ui.BUBBLE_DIMS/2;
+      bubbleCoords.right =+ ui.BUBBLE_DIMS/2;
+    },                        //variables of function
+    getBubbleAngle : function(bubble,e){
+      var mouseCoords = ui.getMouseCoords(e);
+      var bubbleCoords = ui.getBubbleCoords(bubble);
+      var gameCoords = $("#game").position();
+      var boardleft = 120;
+      //Math.atan retrieves angles offset from vertical with positive numbers to the right of
+      //vertical and negative to left of vertical
+      var angle = Math.atan((mouseCoords.x - bubbleCoords.left - boardleft)
+      / (bubbleCoords.top + gameCoords.top - mouseCoords.y));
+      if(mouseCoords.y > bubbleCoords.top + gameCoords.top){
+        angle += Math.PI;
+      }
+      return angle;
     }
   };
   return ui;
